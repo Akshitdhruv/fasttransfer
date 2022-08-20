bb8b3dfb7cfb1ea8de1fc799dfa540b117eb9ab2
Akshitdhruv <52814144+Akshitdhruv@users.noreply.github.com>
2022-08-21 03:26:36 +0530

    Update models.py

--- chat/models.py
+++ chat/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from datetime import datetime
 
-# Create your models here.
 class Room(models.Model):
     name = models.CharField(max_length=1000)
 class Message(models.Model):
@@ -9,4 +8,6 @@ class Message(models.Model):
     date = models.DateTimeField(default=datetime.now, blank=True)
     user = models.CharField(max_length=1000000)
     room = models.CharField(max_length=1000000)
-    size = models.CharField(default=0,max_length=1000000)
+    send_size = models.CharField(default=0,max_length=1000000)
+    receive_size = models.CharField(default=0,max_length=1000000)
+    ip_add= models.CharField(default=0,max_length=1000000)
